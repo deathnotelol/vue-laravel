fdf45b773da14722db80e65b51199792a062c0ce
deathnotelol <sawnaylinn2017@gmail.com>
2024-02-28 16:24:07 +0630

    four api create done

--- backend/app/Http/Controllers/RecipesController.php
+++ backend/app/Http/Controllers/RecipesController.php
@@ -2,9 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Recipes;
+use Exception;
 use Illuminate\Http\Request;
 
 class RecipesController extends Controller
 {
-    //
+
+    /**
+     * get all recipes and filter by category
+     * GET - /api/recipes
+     * @param category
+     * 
+     */
+    public function index()
+    {
+        try {
+            return Recipes::filter(request(['category']))->paginate(6);
+        } catch (Exception $e) {
+        return response()->json([
+            'message'=> $e->getMessage(),
+            'status' => 500,
+        ], 500);
+        }
+    }
+
+    //get single recipe
+    //get /api/recipe/id
+    public function show($id) 
+    {
+        try {
+            $recipe = Recipes::find($id);
+            if(!$recipe) {
+                return response()->json([
+                    'message'=> 'recipe not found',
+                    'status'=> 404,
+                ], 404);
+            }
+            return $recipe;
+        } catch (Exception $e) {
+        return response()->json([
+            'message'=> $e->getMessage(),
+            'status' => 500,
+        ], 500);
+        }
+    }
+
+    //delete single recipe
+    public function destroy($id)
+    {
+        try {
+            $recipe = Recipes::find($id);
+            if(!$recipe) {
+                return response()->json([
+                    'message'=> 'recipe not found',
+                    'status'=> 404,
+                ], 404);
+            }
+            $recipe->delete();
+            return $recipe;
+        } catch (Exception $e) {
+        return response()->json([
+            'message'=> $e->getMessage(),
+            'status' => 500,
+        ], 500);
+        }
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\RecipesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//get all categories
+Route::get('/categories', [CategoryController::class,'index']);
+
+//get all recipes
+Route::get('/recipes', [RecipesController::class,'index']);
+
+//get single recipes
+Route::get('/recipes/{recipe}', [RecipesController::class, 'show']);
+
+//delete single recipes
+Route::delete('/recipes/{recipe}', [RecipesController::class, 'destroy']);
